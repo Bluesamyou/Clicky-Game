6b3caf9025bd47a0d61c404e5b69c5e240c5c493
Shevi <shevids@gmail.com>
2019-10-26 10:30:46 +1100

    Updates

---
+++ precache-manifest.ca8d9cccc9f4b33103e76093146c8223.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d84e9a004160180024913b7f629ddaf",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "0e9c1600e83220cad6ff",
+    "url": "/Clicky-Game/static/css/main.79ca7ab7.chunk.css"
+  },
+  {
+    "revision": "58b512cafb6f17b9a224",
+    "url": "/Clicky-Game/static/js/2.b82b269b.chunk.js"
+  },
+  {
+    "revision": "0e9c1600e83220cad6ff",
+    "url": "/Clicky-Game/static/js/main.79fdc463.chunk.js"
+  },
+  {
+    "revision": "eabcff367a27d49b89d0",
+    "url": "/Clicky-Game/static/js/runtime-main.ca802268.js"
+  }
+]);
